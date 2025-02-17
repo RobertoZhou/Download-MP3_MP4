cabc78c8c916aaf2edaea315824590231f643632
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2025-02-17 00:52:50 -0300

    Atualizando

--- Projeto/conversor_YT.py
+++ Projeto/conversor_YT.py
@@ -1,20 +1,16 @@
-from pytube import YouTube
-print('\033[0;31m       Download De MP4(Videos) e MP3(Áudios)\033[m')
-print('\033[0;34m1\033[m - MP4')
-print('\033[0;34m2\033[m - MP3')
-while True:
-    opcao = input('Digite a Opção de Download: ')
-    if opcao == '1' or opcao == '2':
-        opcao = int(opcao)
-        break
-    print('Opção Selecionada Incorreta. Por Favor escolha um número entre 1 ou 2')
-video = input('\033[0;32mDigite um ou mais links de video do YT\n(\033[0;33mPara baixar mais de um video separa com um "ESPACE"\033[m):\n').split(' ')
-lista_de_link = list(video)
+from pytubefix import YouTube
 
-if opcao == 1:
-    for i in lista_de_link:
-        yt = YouTube(i).streams.get_highest_resolution().download()
+print("Insira a URL dos vídeos que gostaria de baixar")
+print("OBS: Para baixar mais de um vídeo, separe-os com um espaço")
 
-else:
-    for i in lista_de_link:
-        yt = YouTube(i).streams.get_audio_only().download()
+urls_videos = input("URL: ")
+url = urls_videos.split()  # Divide a string em uma lista de URLs usando espaço como delimitador
+
+
+for video in url:
+    yt = YouTube(video)
+    ys = yt.streams.get_highest_resolution()
+    ys.download(output_path=r"ytdownload")
+
+
+
